cf96c5b5e7f3bd506fc0e0dd1e94731674c378e0
Mylena <mylenaleiteb@gmail.com>
2024-05-06 17:30:21 -0300

    Tentando adicionar imagem de fundo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <title>Sistema de Login - Passaporte Industrial Módulo Auditoria</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
